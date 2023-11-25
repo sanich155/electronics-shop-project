768353d2e531e2b5a96bed690148d8f23b8aeb18
Ivan Aladyshkin <sanich1552@gmail.com>
2023-11-25 19:22:56 +0300

    first task

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_Item():
+
+    apple = Item('apple', 55, 70)
+
+    assert apple.name == 'apple'
+    assert apple.price == 55
+    assert apple.quantity == 70
+    assert apple.calculate_total_price() == 70 * 55
+
+    apple.apply_discount()
+    assert apple.price == 55 * 0.85
